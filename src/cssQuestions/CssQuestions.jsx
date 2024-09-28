3ba596c18b457216525fb16f355d5b09b34031dd
Ankit Kumar <ankitrajput10791@gmail.com>
2024-09-28 11:48:56 +0530

    update

---
+++ src/cssQuestions/CssQuestions.jsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from "react";
+
+const CssQuestions = ({ data, searchText, SetSearchText }) => {
+
+    const [showModal,setShowModal] = useState()
+    console.log(data)
+
+    // const filteredData = data.map((item) =>item.cssDataFile.map((item1,index) => item1.Questions.filter((ques,index) => ques.Question.includes(searchText.toLowerCase()))));
+    // console.log(filteredData)
+    
+    const handleModal = () =>{
+
+    }
+    
+    return (
+        <div className="question-container">
+            <h3>css Question</h3>
+            <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
+                <form>
+                    <input type="search" value={searchText} placeholder="Search" onChange={(e) => SetSearchText(e.target.value)} ></input>
+                </form>
+                <button onClick={handleModal} >Add new question</button>
+            </div>
+            {
+                data.map((item, index) => {
+                    return (
+                        <>
+                            {
+                                item.cssDataFile[0].Questions.map((item, index) => {
+                                    return (
+                                        <>
+                                            <h4>Question {index + 1} {item.Question}</h4>
+                                            <p>Answer: {item.Answer}</p>
+                                        </>
+                                    )
+                                })
+                            }
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CssQuestions;
---
+++ src/header/Header.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <div className="headerContainer">
+                <div className="leftHeader">Header</div>
+                <div className="middletHeader"></div>
+                <div className="rightHeader"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;

f746c093af156e091ff472c7600ab3865e43ac33
Satyam Rajput <satyamrajput081998@gmail.com>
2024-09-28 12:50:30 -0400

    side bar data increses

--- src/cssQuestions/CssQuestions.jsx
+++ src/cssQuestions/CssQuestions.jsx
@@ -3,19 +3,35 @@ import './cssQuestions.css';
 
 const CssQuestions = ({ cssData,indexNumber}) => {
     console.log(cssData)
-
+    
     return (
         <div className="question-container">
-            {
-                cssData.map((item) => item.cssDataFile[indexNumber].content.map((cont, index) => {
-                    return (
-                        <>
-                            <h3>{cont.heading}</h3>
-                            <p>{cont.details}</p>
-                        </>
-                    )
-                }))
+    {cssData.map((item) => 
+        item.cssDataFile[indexNumber].content.map((cont) => {
+            console.log(cont);
+            <div>
+                       <h3>{cont.heading}</h3>
+                       <p>{cont.details}</p>
+            </div>
+            if (cont.heading === "CSS Borders") {
+                return cont.details.map((detail) => {
+                    console.log(detail);
+                    return <div>
+                        <img key={detail.url} src={detail.url} />;
+                    </div> 
+                });
+            } else {
+                return (
+                    <>
+                        <h3 key={cont.heading}>{cont.heading}</h3>
+                        <p>{cont.details}</p>
+                    </>
+                );
             }
+        })
+    )
+}
+
 
         </div>
     )

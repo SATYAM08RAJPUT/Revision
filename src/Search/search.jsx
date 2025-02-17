bb2e28c9afeebcfbe7aeb182ed2f2bee7da41f04
Satyam Rajput <satyamrajput081998@gmail.com>
2025-02-17 05:28:12 -0500

    Add some new prototype of search

--- src/Search/search.jsx
+++ src/Search/search.jsx
@@ -1,8 +1,91 @@
+// import React, { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import '../Modal/modal.css';
+// import { CiSearch } from 'react-icons/ci';
+// import useArrowPress from '../CustomHook/useArrowPress';
+
+// const Search = ({ handleCloseModal }) => {
+//   const [input, setInput] = useState('');
+//   const [searchedData, setSearchData] = useState([]);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const fetchApi = async () => {
+//       if (input) {
+//         try {
+//           const response = await fetch(`/api/topics/search?term=${input}`);
+//           const result = await response.json();
+//           setSearchData(result);
+//         } catch (error) {
+//           console.log(error);
+//           setSearchData([]);
+//         }
+//       } else {
+//         setSearchData([]);
+//       }
+//     };
+
+//     const umountTimeout = setTimeout(() => {
+//       fetchApi();
+//     }, 300);
+
+//     return () => {
+//       clearTimeout(umountTimeout);
+//     };
+//   }, [input]);
+
+//   const handleInput = (e) => {
+//     setInput(e.target.value);
+//   };
+
+//   const handleFilterDataTopic = (topic) => {
+//     navigate(`/course/${topic.courseId}/topics/${topic.id}`);
+//     handleCloseModal();
+//   };
+//   const { activeIndex } = useArrowPress(searchedData, handleFilterDataTopic);
+
+//   return (
+//     <div className="modalContainer">
+//       <div className="modal">
+//         <div className="modalHeader">
+//           <div className="searchWrapper">
+//             <CiSearch className="searchIcon" />
+//             <input
+//               type="search"
+//               value={input}
+//               onChange={handleInput}
+//               placeholder="Search docs"
+//             />
+//           </div>
+//           <button onClick={handleCloseModal}>Close</button>
+//         </div>
+//         <ul>
+//           {searchedData.map((topic, index) => (
+//             <li
+//               key={topic.id}
+//               onClick={() => handleFilterDataTopic(topic)}
+//               style={{
+//                 backgroundColor:
+//                   index === activeIndex ? 'hsl(24.6 95% 53.1%)' : '',
+//               }}
+//             >
+//               {topic.title}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Search;
+
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Modal/modal.css';
 import { CiSearch } from 'react-icons/ci';
 import useArrowPress from '../CustomHook/useArrowPress';
+import PropTypes from 'prop-types';
 
 const Search = ({ handleCloseModal }) => {
   const [input, setInput] = useState('');
@@ -76,6 +159,11 @@ const Search = ({ handleCloseModal }) => {
       </div>
     </div>
   );
+};
+
+// PropTypes validation
+Search.propTypes = {
+  handleCloseModal: PropTypes.func.isRequired,
 };
 
 export default Search;
